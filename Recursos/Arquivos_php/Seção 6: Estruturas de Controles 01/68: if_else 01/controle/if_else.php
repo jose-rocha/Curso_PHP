cce61a5920a89824fdf0edec392ba02cedd89427
jose-rocha <jose.rocha@hotmail.com>
2022-03-04 10:07:09 -0300

    add mais arquivos

---
+++ Recursos/Arquivos_php/Seção 6: Estruturas de Controles 01/68: if_else 01/controle/if_else.php
@@ -0,0 +1,7 @@
+<div class="titulo">If Else</div>
+
+<?php
+  if(true) {
+    echo 'Serei impresso?';
+    echo ' <br> Serei impresso novamente?';
+  }
---
+++ Recursos/Arquivos_php/Seção 6: Estruturas de Controles 01/70: Operadores Relacionais 01 e 02/controle/operadores_relacionais.php
@@ -0,0 +1,52 @@
+<div class="titulo">Operadores Relacionais</div>
+
+<?php
+  var_dump(1 == 1);
+  var_dump(1 > 1);
+  var_dump(1 >= 1);
+  var_dump(4 < 23);
+  var_dump(1 <= 7);
+  var_dump(1 != 1); // difrente
+  var_dump(1 <> 1); // difrente, o mesmo sinal de atribuição de diferente no mysql
+
+  echo '<hr>';
+
+  var_dump(111 == '111');
+  var_dump(111 == '111');
+  var_dump(111 != '111');
+  var_dump(111 !== '111');
+
+  echo '<br>';
+  echo '<p >Relacionais no If/Else.</p><hr>';
+  $idade = 31;
+
+  if ($idade < 18) {
+    echo "Menor de idade = $idade anos! <br>";
+  } else if ($idade <= 65) {
+    echo "Adulto = $idade anos! <br>";
+  } else {
+    echo "Terceira idade = $idade anos!";
+  }
+
+  echo '<p>Spaceship</p><hr>';
+  var_dump(500 <=> 3);
+  var_dump(50 <=> 50);
+  var_dump(5 <=> 50);
+
+  echo '<p>Valores pode ser V ou F</p><hr>';
+  var_dump(!!5);
+  var_dump(!!0);
+  var_dump(!!"");
+  var_dump(!!" ");
+?>
+
+  <style>
+    p {
+      margin-bottom: 0px;
+    }
+    hr {
+      margin-top: 0px;
+    }
+  </style>
+
+
