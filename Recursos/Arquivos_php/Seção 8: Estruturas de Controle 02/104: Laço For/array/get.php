29d66b8d8a37042676ba6b9d39c47a08f891910e
José Rocha <josemarlerocha@hotmail.com>
2022-03-20 13:52:31 -0300

    add files

--- Recursos/Arquivos_php/Seção 8: Estruturas de Controle 02/104: Laço For/array/get.php
+++ Recursos/Arquivos_php/Seção 8: Estruturas de Controle 02/104: Laço For/array/get.php
@@ -1,7 +1,7 @@
 <!-- <div class="titulo">$_GET</div> -->
 
 <?php
-echo $_GET . '$_GET é um array vazio'; //$_GET é um array vazio
+echo $_GET; //$_GET é um array vazio
 echo '<br>';
 print_r($_GET);
 echo "<br> {$_GET['nome']}";
---
+++ Recursos/Arquivos_php/Seção 8: Estruturas de Controle 02/104: Laço For/repeticao/for.php
@@ -0,0 +1,1 @@
+<div class="titulo">Laço For</div>
